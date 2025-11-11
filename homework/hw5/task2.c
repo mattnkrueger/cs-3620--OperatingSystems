64833ab674f47a46addb34e6f8699dae97e9a82b
mattnkrueger <mkrue138@gmail.com>
2025-11-10 20:32:31 -0600

    switching to round robin approach worked. now, using read/write threads as specified by the directions to write to solution. mutex still used to access the 'next candidate'

--- homework/hw5/task2.c
+++ homework/hw5/task2.c
@@ -3,13 +3,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <stdbool.h>
+#include <signal.h>
+#include <unistd.h>
 
-typedef struct {
+typedef struct
+{
   unsigned short tid;
   unsigned short challenge;
-  unsigned short nthread;
-} tinput_t;
+  unsigned int nthread;
+} 
+tinput_t;
 
 const unsigned int NSOLUTIONS = 8;
 
@@ -19,16 +22,10 @@ const unsigned int NSOLUTIONS = 8;
 unsigned short found_solutions = 0;
 unsigned long* solutions;
 
-// =============== //
-//  NEXT TO CHECK  //
-// =============== //
-unsigned long next_candidate = 0;
-
 // ======= //
-//  LOCKS  //
+//  MUTEX  //
 // ======= //
-pthread_rwlock_t solutions_lock = PTHREAD_RWLOCK_INITIALIZER;
-pthread_mutex_t work_lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 /**
  * @brief return 0 if number is divisible by [1000000..1500000] otherwise 1
@@ -36,21 +33,22 @@ pthread_mutex_t work_lock = PTHREAD_MUTEX_INITIALIZER;
  * @param n - number to check
  * @return unsigned short 
  */
-unsigned short divisibility_check(unsigned long n) {
+unsigned short divisibility_check(unsigned long n)
+{
   const unsigned long START = 1000000;
   const unsigned long END = 1500000;
-  
+
+  if (n == 0) return 0;
   if (n < START) return 1;
-  if (n <= END) return 0;
-  
-  for (unsigned long i = START; i <= END; i++) 
+
+  for (unsigned long i=START; i<=END; i++)
   {
-    if (n % i == 0) 
+    if (n%i == 0)
     {
       return 0;
     }
   }
-  
+
   return 1;
 }
 
@@ -61,69 +59,68 @@ unsigned short divisibility_check(unsigned long n) {
  * @param attempted_solution - solution to check
  * @return short 
  */
-short try_solution(unsigned short challenge, unsigned long attempted_solution) {
+short try_solution(unsigned short challenge, unsigned long attempted_solution)
+{
   unsigned char digest[SHA256_DIGEST_LENGTH];
   SHA256_CTX ctx;
   SHA256_Init(&ctx);
   SHA256_Update(&ctx, &attempted_solution, 8);
   SHA256_Final(digest, &ctx);
-  
+
   return (*(unsigned short*)digest == challenge) ? 1 : 0;
 }
 
 /**
- * @brief round robin execution across search space by threads
+ * @brief brute forces integers from [0..1000000000] to check against target. Records NSOLUTIONS valid solutions within global solutions array.
  * 
  * @param tinput_void - input thread structure
  * @return void* 
  */
-void* worker_thread_function(void *tinput_void) {
+void* worker_thread_function(void *tinput_void) 
+{
   tinput_t* tinput = (tinput_t*) tinput_void;
-  
-  while (1) {
-    // get candidate to check 
-    pthread_mutex_lock(&work_lock);
-    unsigned long attempted_solution = next_candidate++;
-    pthread_mutex_unlock(&work_lock);
-    
-    // if all solutions already found -> exit
-    pthread_rwlock_rdlock(&solutions_lock);
-    if (found_solutions >= NSOLUTIONS) {
-      pthread_rwlock_unlock(&solutions_lock);
-      break;
-    }
-    pthread_rwlock_unlock(&solutions_lock);
-    
-    // try new solution
-    if (!try_solution(tinput->challenge, attempted_solution)) continue;
+
+  // partition the range of values for each thread
+  const unsigned long RANGE = 1000000000L;
+  unsigned long chunk_size = RANGE / tinput->nthread;
+  unsigned long start = tinput->tid * chunk_size;
+  unsigned long end = (tinput->tid == tinput->nthread - 1) ? RANGE : start + chunk_size;
+
+  for (unsigned long attempted_solution=start; attempted_solution<end; attempted_solution++)
+  {
     if (!divisibility_check(attempted_solution)) continue;
-    
-    pthread_rwlock_wrlock(&solutions_lock);
-    
-    // if all found -> exit
-    if (found_solutions >= NSOLUTIONS) {
-      pthread_rwlock_unlock(&solutions_lock);
-      break;
+    if (!try_solution(tinput->challenge, attempted_solution)) continue;
+
+    // LOCK
+    pthread_mutex_lock(&lock);
+
+    if (found_solutions >= NSOLUTIONS)
+    {
+      pthread_mutex_unlock(&lock);
+      return NULL;
     }
     
-    // BAD SOLUTION - least significant digits match
     short bad_solution = 0;
-    for (int i = 0; i < found_solutions; i++) {
-      if (attempted_solution % 10 == solutions[i] % 10) {
+
+    for (int i=0; i<found_solutions; i++)
+    {
+      if (attempted_solution%10 == solutions[i]%10)
+      {
         bad_solution = 1;
         break;
       }
     }
-    
-    // SOLUTION FOUND
-    if (!bad_solution) {
+
+    if (!bad_solution)
+    {
       solutions[found_solutions] = attempted_solution;
       found_solutions++;
     }
-    
-    pthread_rwlock_unlock(&solutions_lock);
+
+    // UNLOCK
+    pthread_mutex_unlock(&lock);
   }
-  
+
   return NULL;
 }
 
@@ -133,24 +130,25 @@ void* worker_thread_function(void *tinput_void) {
  * @param challenge - target hash prefix (shorts are 2 bytes)
  * @param nthread - thread to solve challenge on
  */
-void solve_one_challenge(unsigned short challenge, unsigned short nthread) {
+void solve_one_challenge(unsigned short challenge, unsigned short nthread)
+{
   pthread_t threads[nthread];
   tinput_t inputs[nthread];
-  
+
   found_solutions = 0;
-  next_candidate = 0;
-  solutions = (unsigned long*) malloc(NSOLUTIONS * sizeof(unsigned long));
-  
-  for (int i = 0; i < NSOLUTIONS; i++) {
+  solutions = (unsigned long*) malloc(NSOLUTIONS * (sizeof(unsigned long)));
+
+  for (int i=0; i<NSOLUTIONS; i++)
+  {
     solutions[i] = 0;
   }
-  
-  // Create worker threads
-  for (int i = 0; i < nthread; i++) {
+
+  for (int i=0; i<nthread; i++)
+  {
     inputs[i].tid = i;
     inputs[i].challenge = challenge;
     inputs[i].nthread = nthread;
-    
+
     pthread_create(                           // CREATE A NEW THREAD
       &(threads[i]),                          // output thread id
       NULL,                                   // optional attribs
@@ -158,47 +156,54 @@ void solve_one_challenge(unsigned short challenge, unsigned short nthread) {
       &(inputs[i])                            // arguments passed to function
     );
   }
-  
-  for (int i = 0; i < nthread; i++) {
+
+  printf("THREADS CREATED!!\n");
+
+  for (int i=0; i<nthread; i++) 
+  {
     pthread_join(threads[i], NULL);
   }
-  
+
+  printf("THREADS COMPLTETED!!\n");
+
   printf("%hu", challenge);
-  for (int i = 0; i < NSOLUTIONS; i++) {
+  for (int i=0; i<NSOLUTIONS; i++)
+  {
     printf(" %lu", solutions[i]);
   }
+
   printf("\n");
-  
   free(solutions);
 }
 
 /**
- * @brief entry point. Ex: `./task 3 12 1000 123`
+ * @brief entry point. Ex: `./task 3 12 1000 123` will use 3 threads to solve the challenges 12, 1000, 123.
  * 
  * @param argc - count of arguments
  * @param argv - contains the number of worker threads (argv[1]) and challenges to solve
  * @return int 
- */
-int main(int argc, char* argv[]) {
-  if (argc < 3)
-  {
-    fprintf(stderr, "Usage: %s <nthreads> <challenge1> [challenge2...]\n", argv[0]);
-    return 1;
-  }
-  
-  unsigned short nthread = strtol(argv[1], NULL, 10);
+j */
+int main(int argc, char* argv[]) 
+{
+  unsigned short nthread = strtol(argv[1], NULL, 10);   
 
-  bool valid = (nthread > 0 && nthread <= 100) ? true : false;
+  bool valid = (nthread <= 100 && nthread > 0) ? true : false; 
   if (!valid) 
   {
-    fprintf(stderr, "Number of threads (%hu) must be between [1-100]", nthread);
-    return 1;
+    printf("Invalid amount of threads [%hu]; Number of threads must be between 1 and 100", nthread);
+    return 0;
   }
-  
-  for (int i = 2; i < argc; i++) {
+
+  printf("Starting Program\n");  
+  printf("NUMBER OF THREADS: %hu\n", nthread);
+
+  for (int i=2; i<argc; i++)
+  {
     unsigned short challenge = strtol(argv[i], NULL, 10);
+
+    printf("\n NEW CHALLENGE: %hu\n", challenge);
     solve_one_challenge(challenge, nthread);
   }
-  
+
   return 0;
 }
